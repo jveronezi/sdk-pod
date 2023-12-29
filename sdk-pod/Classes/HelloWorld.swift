8667790b5357dda3ba2248f046fcb6a4052f523b
jveronezi <jveronezi@fullstacklabs.co>
2023-12-29 15:00:04 -0300

    Adding a new class

---
+++ sdk-pod/Classes/HelloWorld.swift
@@ -0,0 +1,14 @@
+//
+//  HelloWorld.swift
+//  sdk-pod
+//
+//  Created by Joao victor Veronezi on 29/12/23.
+//
+
+import Foundation
+
+public class HelloWorld {
+    public static func sayHello() -> String {
+        return "Hello World"
+    }
+}
