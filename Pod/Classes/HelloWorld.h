0548ba3953c13bef4a762df41334086b32fe93ad
jveronezi <jveronezi@fullstacklabs.co>
2024-01-03 14:16:04 -0300

    Using objc

---
+++ Pod/Classes/HelloWorld.h
@@ -0,0 +1,14 @@
+//
+//  HelloWorld.h
+//  Pods
+//
+//  Created by Joao victor Veronezi on 03/01/24.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HelloWorld : NSObject
+
+- (void)printHelloWorld;
+
+@end
---
+++ Pod/Classes/HelloWorld.m
@@ -0,0 +1,16 @@
+//
+//  HelloWorld.m
+//  CocoaAsyncSocket
+//
+//  Created by Joao victor Veronezi on 03/01/24.
+//
+
+#import "HelloWorld.h"
+
+@implementation HelloWorld
+
+- (void)printHelloWorld {
+    NSLog(@"Hello, World");
+}
+
+@end
